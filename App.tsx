d2b5265e183fde15c74a125e73916e21ff34ddd3
Elmar van Riet <elmarvr@Elmars-MacBook-Pro.local>
2023-10-18 00:13:03 +0200

    add tamagui

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+
+import config from "./tamagui.config";
+import { Button, TamaguiProvider, Theme, styled } from "tamagui";
+import { SafeAreaProvider, SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+export const SafeAreaView = styled(RNSafeAreaView, {
+  name: "SafeAreaView",
+  flex: 1,
+  backgroundColor: "$backgroundStrong",
+});
 
 export default function App() {
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+
+  if (!loaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <TamaguiProvider config={config}>
+        <Theme name="dark">
+          <SafeAreaView>
+            <Button>Click me</Button>
+          </SafeAreaView>
+        </Theme>
+      </TamaguiProvider>
+    </SafeAreaProvider>
   );
+}
+
+{
+  /* <StatusBar style="auto" /> */
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ tamagui.config.ts
@@ -0,0 +1,12 @@
+import { config as configPreset } from "@tamagui/config/v2";
+import { createTamagui } from "tamagui";
+
+const config = createTamagui(configPreset);
+
+// this makes typescript properly type everything based on the config
+type Config = typeof config;
+declare module "tamagui" {
+  interface TamaguiCustomConfig extends Config {}
+}
+
+export default config;
